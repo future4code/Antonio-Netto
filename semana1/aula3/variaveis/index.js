7f142d31cf6971af3b9c5dca8aae9af35a3f23fb
Antonio Machado <antoniomachado__@hotmail.com>
2021-05-03 17:54:59 -0300

    criei-o-arquivo-index.html-2-index.js

---
+++ semana1/aula3/variaveis/index.js
@@ -0,0 +1,45 @@
+/* Analise o programa abaixo e diga o que será impresso no console, SEM EXECUTAR o programa. Resposta: 10,10,5
+
+Analise o programa abaixo e diga o que será impresso no console, SEM EXECUTAR o programa. Resposta: 10,10,10
+
+Analise o programa abaixo, entenda o que ele faz e sugira melhores nomes para as variáveis. Lembre-se que devemos escolher nomes significativos, usar o padrão camelCase. Alem disso, os nomes não podem começar com números ou caracteres especiais.
+Resposta: p = numeroDeHorasTrabalhadas
+t = valorRecebidoPorDia /*
+
+
+
+
+
+
+
+/* e) Pergunte ao usuário seu nome e sua idade, atribuindo esses dois valores às variáveis que acabou de criar.
+
+- 💡  Dica
+
+    Se você receber um erro de código nessa etapa, reflita sobre o tipo de declaração que utilizou.
+
+f) Novamente, imprima na tela o tipo dessas variáveis. O que você notou? Escreva em um comentário de código.
+
+g) Para finalizar, imprima na tela a mensagem: "Olá `nome`,  você tem `idade` anos". Onde `nome` e `idade` são os valores que o usuário inseriu
+
+- 💡  Dica
+
+    Dica: para imprimir mais de um valor na mesma linha, você pode usar essa sintaxe:
+    `console.log(valor1, valor2)` */
+    
+let Nome
+
+let Idade
+
+console.log(typeof Nome)
+
+Nome = prompt("Qual é o seu nome?")
+Idade = prompt("Qual é a sua idade?")
+
+console.log("O tipo da variável Nome", typeof Nome)
+console.log("O tipo da variável Idade", typeof Idade)
+
+console.log("Olá nome,  você tem idade anos")
+
+/* Deu que o tipo é object, não tenho ideia do que é isso e estou completamente perdido /*
+
