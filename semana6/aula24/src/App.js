ac6352ffbb19660ebacc5713131e29b3fa4106da
Antonio Machado <antoniomachado__@hotmail.com>
2021-06-17 15:13:43 -0300

    Aula25-26-27

--- semana6/aula24/src/App.js
+++ semana6/aula24/src/App.js
@@ -1,33 +1,41 @@
 import React from "react";
 import TelaCadastro from "./components/TelaCadastro";
-import TelaListaUsuario from "./components/TelaCadastro";
+import TelaListaUsuarios from "./components/TelaListaUsuarios";
 
 export default class App extends React.Component {
-    state = {
-      telaAtual: "cadastro"
+  state = {
+    telaAtual: "cadastro"
+  }
+
+  escolheTela = () => {
+    switch (this.state.telaAtual) {
+      case "cadastro":
+        return <TelaCadastro irParaLista={this.irParaLista} />
+      case "lista":
+        return <TelaListaUsuarios irParaCadastro={this.irParaCadastro} />
+      default:
+        return <div>Erro! Página não encontrada </div>
     }
+  }
 
-    escolherTela = () => {
-      switch (this.state.telaAtual){
-        case "cadastro":
-          return <TelaCadastro />
-          case "lista":
-            return <TelaListaUsuario />
-            default:
-              return <div>Erro! Página não encontrada </div>
-      }
-    }
+  irParaCadastro = () => {
+    this.setState({ telaAtual: "cadastro" })
+  }
 
-    render () {
-      return (
-        <div>
-         {this.escolherTela ()}
+  irParaLista = () => {
+    this.setState({ telaAtual: "lista" })
+  }
 
-        </div>
-      )
-    }
+  render() {
+    return (
+      <div>
+        {this.escolheTela()}
 
-  
+      </div>
+    )
+  }
+
+
 }
 
 
